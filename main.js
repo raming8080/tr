5d9aa2c79d73c4b7da4feaf54e4e60fcadbb1d0e
Raul Fimbrez <raulfimbrez@yourcsd.com>
2022-11-21 14:38:14 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $("head").append(
+    '<link rel="stylesheet" href="https://drive.google.com/uc?export=view&id=1VEKM0Pw_3nri3bQP1SAHDKC9uoyP0Sps" type="text/css" />'
+  );
+});
