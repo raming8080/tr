d8c381f8e981d94d96a32a3c2fcc6b3a7196a721
raming8080 <rfimbrez@gmail.com>
2022-11-21 16:10:16 -0800

    Update main.js
    updated to githack redirect

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   $("head").append(
-    '<link rel="stylesheet" href="https://raw.githubusercontent.com/raming8080/tr/main/main.css" type="text/css" />'
+    '<link rel="stylesheet" href="https://raw.githack.com/raming8080/tr/main/main.css" type="text/css" />'
   );
 });
