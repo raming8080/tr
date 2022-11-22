1d7d0d9a92495c4fa9bda709c2988a9a5e496710
Raul Fimbrez <raulfimbrez@yourcsd.com>
2022-11-21 16:54:01 -0800

    testing bc

--- classic.js
+++ classic.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   $("head").append(
-    '<link rel="stylesheet" href="" />'
+    '<link rel="stylesheet" href="https://raw.githack.com/raming8080/tr/main/classic.css" />'
   );
 });
